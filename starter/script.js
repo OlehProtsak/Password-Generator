0ecc1c4e6a8d93eaceaa8ea8f115ef080c02cb7d
Oleh Protsak <protsak19@gmail.com>
2023-11-16 19:11:47 +0000

    Implemented getPasswordOptions function

--- starter/script.js
+++ starter/script.js
@@ -88,19 +88,43 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+let finalArrOfChar = [];
+let amountOfChar = 0;
+
 // Function to prompt user for password options
 function getPasswordOptions() {
+    let specialChar = prompt('Do you want to use SPECIAL CHARACTERS in your password? (YES/NO)');
+    let lowrCaseChar = prompt('Do you want to use LOWER CASE CHARACTERS in your password? (YES/NO)');
+    let upperCaseChar = prompt('Do you want to use UPPER CASE CHARACTERS in your password? (YES/NO)');
+    let numbers = prompt('Do you want to use NUMBERS in your password? (YES/NO)');
+    amountOfChar = Number(prompt('Please enter the amount of character you want to use in your password? (from 8 to 128)'));
 
+    if (specialChar === 'YES') {
+      finalArrOfChar.push(...specialCharacters);
+    }
+    if (lowrCaseChar === 'YES') {
+      finalArrOfChar.push(...lowerCasedCharacters);
+    }
+    if (upperCaseChar === 'YES') {
+      finalArrOfChar.push(...upperCasedCharacters);
+    }
+    if (numbers === 'YES') {
+      finalArrOfChar.push(...numericCharacters);
+    }
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  let password = [];
+    for (let i = 0; i < arr.length; i++) {
+      password.push(arr[i]);
+    }
+    return password.join('');
 }
 
 // Function to generate password with user input
 function generatePassword() {
-
+    return getRandom([1,2,3,4,5,6,7]);
 }
 
 // Get references to the #generate element
@@ -116,3 +140,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+getPasswordOptions();
+console.log(amountOfChar, finalArrOfChar);
